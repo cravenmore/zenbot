57f8be80209988725e009231726e19a0d4aed591
Raymond Detwiler <ray@45press.com>
2018-01-09 14:28:30 -0500

    period update

--- extensions/strategies/buy_at_sell_at/strategy.js
+++ extensions/strategies/buy_at_sell_at/strategy.js
@@ -11,7 +11,7 @@ module.exports = function container (get, set, clear) {
 
     getOptions: function () {
       this.option('period', 'period length, same as --periodLength', String, '2m')
-      this.option('periodLength', 'period length, same as --period', String, '2m')
+      this.option('period_length', 'period length, same as --period', String, '2m')
       this.option('min_periods', 'min. number of history periods', Number, 30)
       this.option('buyat', 'Buy when the price surges to x% of the bought price or current balance on initial start (e.g. 1.15 for 15%).', Number, '1.05')
       this.option('sellat', 'Buy again if the price goes down to u% (0.97 for 3%) of the sold price.', Number, '0.97')
